9a6eff7f69f80c2341a17ba81204016713736da8
Graham Knop <haarg@haarg.org>
2014-02-10 00:34:06 -0500

    initial commit

---
+++ lib/Devel/ClassifyWarnings.pm
@@ -0,0 +1,41 @@
+package Devel::ClassifyWarnings;
+use strict;
+use warnings FATAL => 'all';
+
+our $VERSION = '0.001000';
+$VERSION = eval $VERSION;
+
+1;
+__END__
+
+=head1 NAME
+
+Devel::ClassifyWarnings - New module
+
+=head1 SYNOPSIS
+
+  use Devel::ClassifyWarnings;
+
+=head1 DESCRIPTION
+
+New module.
+
+=head1 AUTHOR
+
+haarg - Graham Knop (cpan:HAARG) <haarg@haarg.org>
+
+=head1 CONTRIBUTORS
+
+None yet.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2014 the Devel::ClassifyWarnings L</AUTHOR> and L</CONTRIBUTORS>
+as listed above.
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms
+as perl itself.
+
+=cut
---
+++ t/basic.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings FATAL => 'all';
+use Test::More;
+use Devel::ClassifyWarnings;
+
+ok 0;
+
+done_testing;
